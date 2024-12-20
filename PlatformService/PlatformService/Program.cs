6b4de6743817fcd7f239056eada8969c9e7e9be1
azizanhaniff <45766845+azizanhaniff@users.noreply.github.com>
2024-12-20 08:01:42 +0800

    Data Layer - Repository

--- PlatformService/PlatformService/Program.cs
+++ PlatformService/PlatformService/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseInMemoryDatabase("InMem");
 });
 
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
